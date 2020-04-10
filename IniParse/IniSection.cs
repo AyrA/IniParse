4ac368fcff2ebf3c49cf3a1eb837507a940c90f7
AyrA <admin@ayra.ch>
2020-04-10 11:03:06 +0200

    INI Section comments

--- IniParse/IniSection.cs
+++ IniParse/IniSection.cs
@@ -7,14 +7,32 @@ using System.Threading.Tasks;
 
 namespace IniParse
 {
+    /// <summary>
+    /// Represents a section in an ini file
+    /// </summary>
     public class IniSection
     {
+        /// <summary>
+        /// Gets or sets the name of the section
+        /// </summary>
+        /// <remarks>This can be null for an unnamed section</remarks>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets the list of settings
+        /// </summary>
         public List<IniSetting> Settings { get; private set; }
 
+        /// <summary>
+        /// Gets or sets the section header comments
+        /// </summary>
         public string[] Comments { get; set; }
 
+        /// <summary>
+        /// Gets the setting with the given name
+        /// </summary>
+        /// <param name="name">Setting name</param>
+        /// <returns>Setting</returns>
         public IniSetting this[string name]
         {
             get
@@ -23,6 +41,11 @@ namespace IniParse
             }
         }
 
+        /// <summary>
+        /// Gets the setting at the given position
+        /// </summary>
+        /// <param name="index">Setting position</param>
+        /// <returns>Setting</returns>
         public IniSetting this[int index]
         {
             get
@@ -31,17 +54,30 @@ namespace IniParse
             }
         }
 
+        /// <summary>
+        /// Initializes an empty section
+        /// </summary>
         public IniSection() : this(null)
         {
 
         }
 
+        /// <summary>
+        /// Initializes a named section
+        /// </summary>
+        /// <param name="SectionName">Section name</param>
         public IniSection(string SectionName)
         {
             Name = SectionName;
             Settings = new List<IniSetting>();
         }
 
+        /// <summary>
+        /// Exports this section
+        /// </summary>
+        /// <param name="SW">Location to write contents to</param>
+        /// <param name="CommentChar">Character for comments</param>
+        /// <returns>Task</returns>
         public async Task ExportSection(StreamWriter SW, char CommentChar)
         {
             if (Name != null)
