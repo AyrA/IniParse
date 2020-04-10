ef1b9de844accf4899869e9f1f74c13e1ba9114b
AyrA <admin@ayra.ch>
2020-04-10 12:21:11 +0200

    Add comments to tools

--- IniParse/Tools.cs
+++ IniParse/Tools.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace IniParse
 {
+    /// <summary>
+    /// Helper functions for the INI parser
+    /// </summary>
     internal class Tools
     {
+        /// <summary>
+        /// Checks if a given array is null or is empty
+        /// </summary>
+        /// <param name="a">Array</param>
+        /// <returns><paramref name="a"/>==null || <paramref name="a"/>.Length==0</returns>
         public static bool IsEmpty(Array a)
         {
             return a == null || a.Length == 0;
