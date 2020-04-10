c8904eb5be5c547a85c1eb249f1019900c967f74
AyrA <admin@ayra.ch>
2020-04-10 11:05:19 +0200

    INI setting comments

--- IniParse/IniSetting.cs
+++ IniParse/IniSetting.cs
@@ -6,23 +6,50 @@ using System.Threading.Tasks;
 
 namespace IniParse
 {
+    /// <summary>
+    /// Represents a single setting in an INI file
+    /// </summary>
     public class IniSetting
     {
+        /// <summary>
+        /// Gets or sets the comments for this setting
+        /// </summary>
         public string[] Comments { get; set; }
+        /// <summary>
+        /// Gets or sets the name of this setting
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the value of this setting
+        /// </summary>
         public string Value { get; set; }
 
+        /// <summary>
+        /// Initializes an empty setting
+        /// </summary>
         public IniSetting()
         {
-
+            Name = "";
+            Value = "";
         }
 
+        /// <summary>
+        /// Initilizes a given setting
+        /// </summary>
+        /// <param name="SettingName">Setting name</param>
+        /// <param name="SettingValue">Setting value</param>
         public IniSetting(string SettingName, string SettingValue)
         {
             Name = SettingName;
             Value = SettingValue;
         }
 
+        /// <summary>
+        /// Exports this setting
+        /// </summary>
+        /// <param name="SW">Location to write contents to</param>
+        /// <param name="CommentChar">Character for comments</param>
+        /// <returns>Task</returns>
         public async Task ExportSetting(StreamWriter SW, char CommentChar)
         {
             if(!Tools.IsEmpty(Comments))
